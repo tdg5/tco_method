ca78a1a571a05a08f09dee708d4b16e68b275d19
Danny Guinther <dannyguinther@gmail.com>
2015-03-09 07:37:42 -0400

    Relocate module test

--- test/unit/tco_method_module_test.rb
+++ test/unit/tco_method_module_test.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 
-class TCOMethodTest < TCOMethod::TestCase
-  SUBJECT = TCOMethod
+class TCOMethodModuleTest < TCOMethod::TestCase
+  Subject = TCOMethod
 
-  context SUBJECT.name do
-    subject { SUBJECT }
+  context Subject.name do
+    subject { Subject }
 
     should "be defined" do
       assert defined?(subject), "Expected #{subject.name} to be defined!"
