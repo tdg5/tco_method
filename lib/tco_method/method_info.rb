bfb7630725e58ed4298dfed80768aabeb41c04e2
Danny Guinther <dannyguinther@gmail.com>
2015-03-12 08:00:14 -0400

    Add more type checking and tests to MethodInfo

--- lib/tco_method/method_info.rb
+++ lib/tco_method/method_info.rb
@@ -1,7 +1,19 @@
 module TCOMethod
+  # Class encapsulating the behaviors required to extract information about a
+  # method from the 14-element Array of data representing the instruction
+  # sequence of that method.
   class MethodInfo
-    def initialize(method)
-      @info = RubyVM::InstructionSequence.of(method).to_a
+    VALID_METHOD_CLASSES = [
+      Method,
+      UnboundMethod,
+    ].freeze
+
+    def initialize(method_obj)
+      unless VALID_METHOD_CLASSES.any? { |klass| method_obj.is_a?(klass) }
+        msg = "Invalid argument! Method or UnboundMethod expected, received #{method_obj.class.name}"
+        raise TypeError, msg
+      end
+      @info = RubyVM::InstructionSequence.of(method_obj).to_a
     end
 
     def type
--- test/unit/method_info_test.rb
+++ test/unit/method_info_test.rb
@@ -4,18 +4,61 @@ class MethodInfoTest < TCOMethod::TestCase
   Subject = TCOMethod::MethodInfo
 
   TestClass = Class.new do
-    def def_method; end
-    define_method(:block_method) { }
+    class << self; define_method(:class_block_method) { }; end
+    def self.class_def_method; end
+    define_method(:instance_block_method) { }
+    def instance_def_method; end
+  end
+
+  context "#initialize" do
+    should "raise TypeError unless given a Method object" do
+      non_methods = [
+        proc { },
+        lambda { },
+        Proc.new { },
+      ]
+      non_methods.each do |non_method|
+        assert_raises(TypeError) do
+          Subject.new(non_method)
+        end
+      end
+    end
+
+    should "accept Method objects defined on a class using def" do
+      method_obj = TestClass.method(:class_def_method)
+      assert_kind_of Method, method_obj
+      assert_kind_of Subject, Subject.new(method_obj)
+    end
+
+    should "accept UnboundMethod objects defined on an instance using def" do
+      method_obj = TestClass.instance_method(:instance_def_method)
+      assert_kind_of UnboundMethod, method_obj
+      assert_kind_of Subject, Subject.new(method_obj)
+    end
+
+    should "accept Method objects defined on a class using define_method" do
+      method_obj = TestClass.method(:class_block_method)
+      assert_kind_of Method, method_obj
+      assert_kind_of Subject, Subject.new(method_obj)
+    end
+
+    should "accept UnboundMethod objects defined on an instance using define_method" do
+      method_obj = TestClass.instance_method(:instance_block_method)
+      assert_kind_of UnboundMethod, method_obj
+      assert_kind_of Subject, Subject.new(method_obj)
+    end
   end
 
   context "#type" do
     subject { TestClass.new }
     should "return :method for methods defined using def" do
-      assert_equal :method, Subject.new(subject.method(:def_method)).type
+      assert_equal :method, Subject.new(TestClass.method(:class_def_method)).type
+      assert_equal :method, Subject.new(subject.method(:instance_def_method)).type
     end
 
     should "return :block for methods defined using define_method" do
-      assert_equal :block, Subject.new(subject.method(:block_method)).type
+      assert_equal :block, Subject.new(TestClass.method(:class_block_method)).type
+      assert_equal :block, Subject.new(subject.method(:instance_block_method)).type
     end
   end
 end
